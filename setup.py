25445e799dcea42765cbc6dbb257d146493061cd
massongit <suzukimasaya428@gmail.com>
2020-08-09 22:32:06 +0900

    install_requiresに指定するパッケージをPipfileから取得するよう変更

--- setup.py
+++ setup.py
@@ -6,7 +6,20 @@ from setuptools import setup
 
 
 def _requires_from_file(filename):
-    return open(filename).read().splitlines()
+    is_in_packages = False
+    requires = []
+
+    with open(filename) as f:
+        for r in f:
+            r = r.strip()
+            if r == '[packages]':
+                is_in_packages = True
+            elif r.startswith('['):
+                is_in_packages = False
+            elif r and is_in_packages:
+                requires.append(r.replace('"', '').replace(' ', '').replace('=', '', 1))
+
+    return requires
 
 
 setup(
@@ -16,7 +29,7 @@ setup(
     description="突然の死(ハリフキダシ)を生成するツール",
     author="koluku",
     url="https://github.com/koluku/sudden-death",
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=_requires_from_file('Pipfile'),
     packages=['sudden_death'],
     package_data={
         "sudden_death": ["py.typed"],
